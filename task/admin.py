3f7eb20e6244651aa3f379ef2fc345af1b80abc5
Viacheslav Shcherbak <misternictof@gmail.com>
2023-03-21 13:36:05 +0200

    feature registered models TaskType, Position, Worker, Priority, Task, Team, Project on the admin pageÐ

--- task/admin.py
+++ task/admin.py
@@ -1,4 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
+from task.models import (
+    Position,
+    Team,
+    Priority,
+    TaskType,
+    Project,
+    Task,
+    Worker
+)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                        "email",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+admin.site.register(TaskType)
+admin.site.register(Team)
+admin.site.register(Priority)
+admin.site.register(Position)
 admin.site.unregister(Group)
