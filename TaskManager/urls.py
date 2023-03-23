acb88cd5aa3412dcf1b2dc22f30a58e3b868e99c
Viacheslav Shcherbak <misternictof@gmail.com>
2023-03-23 20:50:02 +0200

    feature add path to account & django-debug-toolbar in TaskManager/urls.py

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task.urls", namespace="task")),
-]
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('__debug__/', include('debug_toolbar.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
