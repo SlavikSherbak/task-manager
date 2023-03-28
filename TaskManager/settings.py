e1d4d859f5b541de109eae5e04ddf5253b29865c
Viacheslav Shcherbak <misternictof@gmail.com>
2023-03-28 16:29:37 +0300

    add render.com to Allowed hosts

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "task-manager-tafz.onrender.com"]
 
 # Application definition
 
