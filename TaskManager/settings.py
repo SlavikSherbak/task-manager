e05a41ef96b0c96643cbda3ac3ea851864d7f3fd
Viacheslav Shcherbak <misternictof@gmail.com>
2023-03-28 15:15:37 +0300

    remove insecure Django secret key from settings

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-uvgz8ih3%tnupqf30mfz^&(pz6*k55wkl6cj2&+9(3%f#6x&^5")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
